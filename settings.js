584fb8413efde594897f9fa2e9a93c24edbbc64d
Thariyaavishka <thariyaavishka@gmail.com>
2025-07-29 19:40:45 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
+  SESSION_ID: "SUBZERO~44af8d4d1dbb1e6c0ec98e54f03d8704f5c7469f", // Put Session Id Here Inside "*"
+  OWNER_NUMBER: "94770614128", // Put Owner Number Here 
   PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
 };
 
